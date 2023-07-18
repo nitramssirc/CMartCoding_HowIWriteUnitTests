959420cb7d4b7b80e36a94f231ee4f332c98b45f
Criss Martin <nitramssirc@yahoo.com>
2023-07-17 22:32:43 -0400

    saved

--- Class1.cs
+++ Class1.cs
@@ -2,6 +2,16 @@
 {
     public class Class1
     {
-
+        /*
+         * Tests to Create
+         * Simple output Assert
+         * Test with parameters
+         * Test with mock
+         * Test with event handler
+         * Test that fails
+         * Test expecting an exception
+         * Tests verifying a call
+         * Test with mock setup
+         */
     }
 }
